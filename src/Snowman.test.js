e4d4e5c04685dbc61a34c55feff6f28bedbc0260
Claudia Lam <claudiaslam@gmail.com>
2023-04-19 19:26:15 -0700

    change word from apple to english words

--- src/Snowman.test.js
+++ src/Snowman.test.js
@@ -1,11 +1,12 @@
 import { render, fireEvent } from "@testing-library/react";
 import Snowman from "./Snowman";
 
-describe("End Game display", function (){
+describe("End Game display", function () {
   test("it stops when maxGuess is reached", function () {
-    const snowmanGame = render(<Snowman />);
+    const snowmanGame = render(<Snowman words={["apple"]} />);
 
     console.log("snowmanGame out of loop", snowmanGame);
+    console.log("snowmanGame-text", snowmanGame.getByText("z"));
 
     const wrongGuesses = ["n", "b", "c", "q", "z", "t"];
     //click  wrong guesses
@@ -15,8 +16,15 @@ describe("End Game display", function (){
       fireEvent.click(ltrButton);
     }
 
-    expect(snowmanGame.getByText("You lose! apple")).toBeInTheDocument();
+    expect(snowmanGame.queryByText("You lose! apple")).toBeInTheDocument();
+    expect(
+      snowmanGame.container.querySelector('img[alt="6"]')
+    ).toBeInTheDocument();
+    expect(snowmanGame.queryByText("z")).not.toBeInTheDocument();
   });
-})
+});
 
-
+it("matches snapshot", function () {
+  const { container } = render(<Snowman />);
+  expect(container).toMatchSnapshot();
+});
